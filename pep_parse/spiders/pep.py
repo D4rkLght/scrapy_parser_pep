4d528a6e7842a8ef6a223add427b241006e3154e
Ярослав Андреев <1frost34eone@gmail.com>
2023-07-02 16:53:23 +0300

    parsed pep and did items

---
+++ pep_parse/spiders/pep.py
@@ -0,0 +1,22 @@
+import scrapy
+
+from pep_parse.items import PepParseItem
+
+
+class PepSpider(scrapy.Spider):
+    name = 'pep'
+    allowed_domains = ['peps.python.org']
+    start_urls = ['http://peps.python.org/']
+
+    def parse(self, response):
+        pep_links = response.css('section#numerical-index a.pep::attr(href)').getall()
+        for pep_link in pep_links:
+            yield response.follow(pep_link, callback=self.parse_pep)
+    
+    def parse_pep(self, response):
+        data = {
+            "number": int(response.css('h1.page-title::text').get().split()[1]),
+            "name": ' '.join(response.css('h1.page-title::text').get().split()[3:]),
+            "status": response.css('dt:contains("Status") + dd abbr::text').get()
+        }
+        yield PepParseItem(data)
